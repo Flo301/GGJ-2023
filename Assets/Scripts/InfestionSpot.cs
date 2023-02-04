be4b6950d47ba6aa5677d1582685b539ad33e59c
Benjamin Kleiner <bizzl@benkle.info>
2023-02-04 15:06:50 +0100

    Added triggerDistance and ForcedSpawnTime

--- Assets/Scripts/InfestionSpot.cs
+++ Assets/Scripts/InfestionSpot.cs
@@ -15,10 +15,47 @@ public class InfestionSpot : MonoBehaviour
     public int minRewardAmount;
     public int maxRewardAmount;
 
+    public GameObject Player;
+
+    public float triggerDistance;
+
     private List<AttackingEntity> SpawnedEntities = new List<AttackingEntity>();
+
+    public float forceSpawnTime;
+
+    private float deltaTime = 0;
+
+    Transform playerTransform;
 
     void Start()
     {
+        playerTransform = Player.GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        if (SpawnedEntities.Count > 0)
+        {
+            return;
+        }
+        deltaTime += Time.deltaTime;
+        float playerDistance = Vector3.Distance(transform.position, playerTransform.position);
+        if (deltaTime > forceSpawnTime)
+        {
+            //Debug.Log($"{gameObject.name}: {deltaTime} > {forceSpawnTime}");
+            StartInfestation();
+        }
+        else if (playerDistance < triggerDistance)
+        {
+            //Debug.Log($"{gameObject.name}: {distance} < {triggerDistance}");
+            StartInfestation();
+        }
+    }
+
+    void StartInfestation()
+    {
+        Debug.Log($"{gameObject.name}: Start Infestion");
+
         var spawnAmount = Random.Range(minEntityAmount, maxEntityAmount);
 
         for (int i = 0; i < spawnAmount; i++)
@@ -26,7 +63,8 @@ public class InfestionSpot : MonoBehaviour
             var origin = transform.position + Vector3.right * Random.Range(-distance, distance) + Vector3.forward * Random.Range(-distance, distance) + Vector3.up * 100;
             if (Physics.Raycast(origin, Vector3.down, out RaycastHit hit))
             {
-                var enemy = Instantiate(EntityObject, hit.point, Quaternion.identity);
+                Vector3 spawnPosition = new Vector3(hit.point.x, 0, hit.point.z);
+                var enemy = Instantiate(EntityObject, spawnPosition, Quaternion.identity);
                 var entity = enemy.GetComponentInChildren<AttackingEntity>();
                 entity.SetInfestationSpot(this);
                 SpawnedEntities.Add(entity);
